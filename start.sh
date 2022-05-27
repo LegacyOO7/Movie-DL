7333b839f6ae27ce8a4ee2baa82b26993e65f6e3
legion <88172317+LegacyOO7@users.noreply.github.com>
2022-05-27 08:50:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
 git clone -b master https://github.com/LegacyOO7/Movie-DL /Movie-DL
-cd /Movie-Bot
+cd /Movie-DL
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
